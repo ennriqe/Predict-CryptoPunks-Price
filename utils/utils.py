2dc8dd96f3c2a3f389177074222c344d65594221
unknown <e.barrueco96@gmail.com>
2024-02-25 17:01:40 +0100

    Cleaned the code of the notebook and put the functions into the utils file

--- utils/utils.py
+++ utils/utils.py
@@ -51,3 +51,88 @@ def get_most_common_color(image):
     # filtered_counts = counts[is_not_replacement_color]
     # skin_color = filtered_colors[np.argmax(filtered_counts)]
     # return skin_color
+
+import matplotlib.pyplot as plt
+import requests
+import statsmodels.api as sm
+import pandas as pd
+import numpy as np
+
+# Assuming X_test, y_test, and y_pred_gb_lgb are already defined
+
+def plot_scatter_predictions_and_actuals_with_OLS(ax, X_test_subset, title):
+    # Create the scatter plot
+    ax.scatter(X_test_subset['y_test'], X_test_subset['y_pred_gb_lgb'])
+
+    # Compute the OLS regression line without a constant
+    X = X_test_subset['y_test']
+    model = sm.OLS(X_test_subset['y_pred_gb_lgb'], X)
+    results = model.fit()
+    slope = results.params[0]
+
+    # Plot the OLS regression line
+    x_vals = np.array(ax.get_xlim())
+    y_vals = slope * x_vals
+    ax.plot(x_vals, y_vals, 'b-', label='OLS Line')
+
+    # Determine the upper limit for the 45-degree line
+    upper_limit = max(ax.get_xlim()[1], ax.get_ylim()[1])
+
+    # Plot the 45-degree line starting from (0, 0)
+    ax.plot([0, upper_limit], [0, upper_limit], 'r--', alpha=0.75, label='45-degree line')
+
+    # Set the axes to start from (0, 0)
+    ax.set_xlim(left=0)
+    ax.set_ylim(bottom=0)
+    ax.set_ylabel("Model's Prediction")
+    ax.set_xlabel("Actual Value")
+    # Set title and show the legend
+    ax.set_title(title)
+    ax.legend()
+    
+def exclude_top_and_bottom_5_percent(series):
+    # Exclude the lowest 5%
+    lower_threshold = np.percentile(series, 5)
+    filtered_series = series[series > lower_threshold]
+    
+    # Exclude the top 5%
+    upper_threshold = np.percentile(filtered_series, 95)
+    final_filtered_series = filtered_series[filtered_series < upper_threshold]
+    
+    return final_filtered_series
+def plot_eth_and_punk_prices(sales):
+    sales['block_date'] = pd.to_datetime(sales.block_date)
+    sales_sorted = sales.sort_values(by='block_date')
+    mean_amount = sales_sorted.groupby('block_date').amount_original.mean()
+
+    # Create subplots
+    fig, axs = plt.subplots(1, 2, figsize=(12, 5))
+
+    # Plot on the first subplot
+    axs[0].plot(np.log(mean_amount), label='Mean Punk Price in ETH')
+    axs[0].plot(np.log(sales_sorted.groupby('block_date').Ethereum_Price.mean()), label='ETH Price')
+    axs[0].plot(np.log(sales_sorted.groupby('block_date')['CP Price'].mean()), label='Punk Floor in ETH')
+    axs[0].legend()
+
+    axs[0].set_title('Log Prices')
+
+    # Plot on the second subplot
+    axs[1].plot(mean_amount)
+    axs[1].set_title('Mean Punk Price in ETH')
+
+    plt.show()
+
+def get_ethereum_price_history():
+    # URL of the API endpoint (using CoinGecko as an example)
+    url = 'https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=usd&days=max'
+
+    # Make a request to the API
+    response = requests.get(url)
+    data = response.json()
+
+    prices = data['prices']  # This might vary based on the API structure
+    dates = [pd.to_datetime(price[0], unit='ms') for price in prices]
+    eth_prices = [price[1] for price in prices]
+    eth = pd.DataFrame({'Date': dates, 'Ethereum_Price': eth_prices})
+
+    return eth
