a46c98a9e1d9358f6b283f37d69e48be245b65bc
Luca Brugaletta <luca.brugaletta@outlook.com>
2024-02-26 21:36:52 +0100

    Cleaned utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,8 +1,8 @@
 import numpy as np
-from copy import deepcopy
-from PIL import Image
-uniform_background_color = [99, 133, 150]
-black_color = [0, 0, 0]
+import matplotlib.pyplot as plt
+import requests
+import statsmodels.api as sm
+import pandas as pd
 
 
 specified_colors = {
@@ -16,9 +16,27 @@ specified_colors = {
 }
 
 def extract_id(url):
+    """
+    Extracts the id of the punk from the url
+
+    Args:
+    url (str): The url of the punk image
+
+    Returns:
+    str: The id of the punk
+    """
     return url.split('/')[-1].split('.')[0].split('cryptopunk')[-1]
 
 def get_most_common_color(image):
+    """
+    Returns the skin color of the cryptopunk
+
+    Args:
+    image (PIL.Image): The image to process
+
+    Returns:
+    str: The skin label of the punk
+    """
     # Convert the image to a numpy array
     image_array = np.array(image)
     # Reshape the array to a 2D array of pixels and 3 color values (RGB)
@@ -32,31 +50,7 @@ def get_most_common_color(image):
 
     # Find the most frequent specified color
     most_frequent_specified_label = max(color_counts, key=color_counts.get)
-    most_frequent_specified_count = color_counts[most_frequent_specified_label]
     return most_frequent_specified_label
-    # # Convert back to an image
-    # modified_image = Image.fromarray(image_new)    
-    # unique_colors, counts = np.unique(pixels, axis=0, return_counts=True)
-    # most_frequent_color = unique_colors[np.argmax(counts)]
-    # image_array[(image_array == most_frequent_color).all(axis=-1)] = uniform_background_color
-    # unique_colors, counts = np.unique(pixels, axis=0, return_counts=True)
-
-    # # Create masks to filter out the replacement color and black
-    # is_not_replacement_color = ~np.all(unique_colors == uniform_background_color, axis=1)
-    # is_not_black = ~np.all(unique_colors == black_color, axis=1)
-    # is_not_excluded_color = is_not_replacement_color & is_not_black
-
-    # # Filter out the blue background and black [0,0,0] borders
-    # filtered_colors = unique_colors[is_not_excluded_color]
-    # filtered_counts = counts[is_not_replacement_color]
-    # skin_color = filtered_colors[np.argmax(filtered_counts)]
-    # return skin_color
-
-import matplotlib.pyplot as plt
-import requests
-import statsmodels.api as sm
-import pandas as pd
-import numpy as np
 
 # Assuming X_test, y_test, and y_pred_gb_lgb are already defined
 
