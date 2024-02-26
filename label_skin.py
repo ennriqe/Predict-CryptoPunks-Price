05e2467c3e481f9f3b1df4833a4cd12e5e1ba99e
Luca Brugaletta <luca.brugaletta@outlook.com>
2024-02-26 21:50:51 +0100

    changed to skin

--- label_skin.py
+++ label_skin.py
@@ -19,6 +19,6 @@ if __name__ == "__main__":
         image, id, url = image_data['image'], image_data['id'], image_data['image_original_url']
         cpunks_id = extract_id(url)
         skin_color = get_most_common_color(image)
-        df = df.append({'id': cpunks_id, 'skin': skin_color, 'url': url}, ignore_index=True)
+        df = df.append({'id': cpunks_id, 'skin_color': skin_color, 'url': url}, ignore_index=True)
     print(df.head(5))
     df.to_csv("cryptopunks_skin.csv", index=False)
